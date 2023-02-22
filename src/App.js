dec3f2f0e6d1f0f06b63564888a35d7887eb4246
lestercoyoy <52750976+lestercoyoyjr@users.noreply.github.com>
2023-02-21 23:40:06 -0600

    ADD
    API call in order to know info is being loaded

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
-import React from 'react';
+import {useEffect} from 'react';
+
+// api test key 89ce9f35
+
+const API_URL = 'http://omdbapi.com?apikey=89ce9f35';
 
 const App = () => {
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        // For now, just the info from the movies
+        console.log(data.Search);
+    }
+
+    useEffect(() => {
+        searchMovies('Spiderman');
+    }, []);
+    
     return (
         <h1>App</h1>
     );
