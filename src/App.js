0add24541ef605159a3e012b3455b03500355104
lestercoyoy <52750976+lestercoyoyjr@users.noreply.github.com>
2023-02-24 23:59:19 -0600

    MODIFY
    Placeholder image

--- src/App.js
+++ src/App.js
@@ -43,11 +43,15 @@ const App = () => {
                   onClick={() => {}}
                 />
             </div>
-            
+
             <div className='container'>
               <div className='movie'>
                 <div>
                   <p>{movie1.Year}</p>
+                </div>
+
+                <div>
+                    <img src={movie1.Poster !== 'N/A' ? movie1.Poster : 'https://via.placeholder.com/400'} alt={movie1.Title}/>
                 </div>
               </div>
             </div>
